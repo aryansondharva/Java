117822a49c8295cf08de0537f8cc92ad7938edba
ARYAN SONDHARVA <aryansondharva25@gmail.com>
2026-04-21 19:19:30 +0530

    message

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+} 
+
+/*public class Hello {                    // class name = file name
+    public static void main(String[] args) {   // program starts here
+        System.out.println("Hello World");     // print + new line
+    }
+} 
+    public — anyone can access
+class — everything lives inside a class
+main — starting point of program
+System.out.println — prints output   */
