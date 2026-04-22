92a7a9c3bf3f15d18f973488714024ef58dc361e
ARYAN SONDHARVA <aryansondharva25@gmail.com>
2026-04-22 07:36:03 +0530

    22 April 2026 // Day 2

--- Day 1/Input.java
+++ Day 1/Input.java
@@ -19,3 +19,13 @@ public class Input {
         System.out.println("Sum = " + (a + b));
     }
 }
+
+
+
+// import java.util.Scanner;  // bring Scanner tool into your file
+// javaScanner sc = new Scanner(System.in);  // sc = your keyboard listener
+// javaSystem.out.println("Enter number 1: ");  // show message to user
+// int a = sc.nextInt();  // wait for user to type, store in 'a'
+// javaSystem.out.println("Enter number 2: ");
+// int b = sc.nextInt();  // store second number in 'b'
+// javaSystem.out.println("Sum = " + (a + b));  // print result
--- date.md
+++ date.md
@@ -1,1 +1,2 @@
 21 April 2026 // Day 1
+22 April 2026 // Day 2
